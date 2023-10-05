06d8cca4d5c64666d156cab7c890594e674c79dc
Youssef_Ahmed <110913003+Youssefmdany@users.noreply.github.com>
2023-10-06 00:25:56 +0300

    Update Oversized_Packet_Sequence.sv

--- Testbench/Sequences/Oversized_Packet_Sequence.sv
+++ Testbench/Sequences/Oversized_Packet_Sequence.sv
@@ -44,9 +44,9 @@ class Oversized_Packet_Sequence extends Base_Sequence;
 
 		if(!(Bringup_packet.randomize(* solvearrayresizemax = 0 *)() with {
 		
-		                                      reset_156m25_n==1;
-	                                     	  reset_xgmii_rx_n==1;
-	                                     	  wb_rst_i==0;
+		                                                                                                 reset_156m25_n==1;
+	                                     	                                                                 reset_xgmii_rx_n==1;
+	                                     	                                                                  wb_rst_i==0;
 														  reset_xgmii_tx_n==1;
 														  Loaded_Data.size() inside {[1500:9000]};
 														  inter_packet_gap inside {[10:50]}; 
