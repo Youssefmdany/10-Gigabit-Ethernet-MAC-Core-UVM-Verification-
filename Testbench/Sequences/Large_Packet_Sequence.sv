dc940dba45cf3a0cb7a3794ddfb03f689b5167bb
Youssef_Ahmed <110913003+Youssefmdany@users.noreply.github.com>
2023-10-06 00:26:40 +0300

    Update Large_Packet_Sequence.sv

--- Testbench/Sequences/Large_Packet_Sequence.sv
+++ Testbench/Sequences/Large_Packet_Sequence.sv
@@ -44,9 +44,9 @@ class Large_Packet_Sequence extends Base_Sequence;
 		
 		if(!(Bringup_packet.randomize() with {
 		
-		                                      reset_156m25_n==1;
-	                                     	  reset_xgmii_rx_n==1;
-	                                     	  wb_rst_i==0;
+		                                      		                                                  reset_156m25_n==1;
+	                                     	      		                                                  reset_xgmii_rx_n==1;
+	                                     	    		                                                  wb_rst_i==0;
 														  reset_xgmii_tx_n==1;
 														  Loaded_Data.size() inside {[1450:1500]};
 														  inter_packet_gap inside {[10:50]}; 
