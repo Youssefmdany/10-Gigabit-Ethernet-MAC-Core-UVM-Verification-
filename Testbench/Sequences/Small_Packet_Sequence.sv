d9e02ed1d6883b09b6bcf362a4aa8a845d7046bd
Youssef_Ahmed <110913003+Youssefmdany@users.noreply.github.com>
2023-10-06 00:27:21 +0300

    Update Small_Packet_Sequence.sv

--- Testbench/Sequences/Small_Packet_Sequence.sv
+++ Testbench/Sequences/Small_Packet_Sequence.sv
@@ -44,9 +44,9 @@ class Small_Packet_Sequence extends Base_Sequence;
 		
 		if(!(Bringup_packet.randomize() with {
 		
-		                                      reset_156m25_n==1;
-	                                     	  reset_xgmii_rx_n==1;
-	                                     	  wb_rst_i==0;
+		                                    								  reset_156m25_n==1;
+	                                   								    	  reset_xgmii_rx_n==1;
+	                                 								      	  wb_rst_i==0;
 														  reset_xgmii_tx_n==1;
 														  Loaded_Data.size() inside {[46:50]};
 														  inter_packet_gap inside {[10:50]}; 
